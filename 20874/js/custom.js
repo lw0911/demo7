ab9b6570b4758f9e2fdd382f9c9cd860cfb7cb95
懒之才_测试文件 <demo1@aizhanzhe.com>
2021-08-16 20:33:48 +0800

    更新文件

---
+++ 20874/js/custom.js
@@ -0,0 +1,38 @@
+(function () {
+
+	'use strict'
+
+
+	AOS.init({
+		duration: 800,
+		easing: 'slide',
+		once: true
+	});
+
+	var preloader = function() {
+
+		var loader = document.querySelector('.loader');
+		var overlay = document.getElementById('overlayer');
+
+		function fadeOut(el) {
+			el.style.opacity = 1;
+			(function fade() {
+				if ((el.style.opacity -= .1) < 0) {
+					el.style.display = "none";
+				} else {
+					requestAnimationFrame(fade);
+				}
+			})();
+		};
+
+		setTimeout(function() {
+			fadeOut(loader);
+			fadeOut(overlay);
+		}, 200);
+	};
+	preloader();
+
+
+	
+
+})()
